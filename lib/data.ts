8f2d1688d4634ffb58f551f7ed433eb98438791e
AyoubAchour <medayoubachour69@gmail.com>
2023-11-13 16:39:47 +0100

    Updated files related to Vercel deployment

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import hilink from "@/public/hilink.png";
-import priceWise from "@/public/priceWise.png";
-import snapGram from "@/public/snapGram.png";
+import hilink from "../public/hilink.png";
+import priceWise from "../public/priceWise.png";
+import snapGram from "../public/snapGram.png";
 
 
 export const links = [
